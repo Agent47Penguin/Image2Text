686c04b44ee34d937aa77748905d6777fe8167ea
Agent47Penguin <nikspenguins@gmail.com>
2023-03-13 13:26:44 -0700

    Added Open Image Button Functionality

--- image2text.py
+++ image2text.py
@@ -2,6 +2,7 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QTextEdit, QPushButton, QFileDialog, QVBoxLayout, QHBoxLayout, QWidget
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt
+from PIL import Image
 
 # MainWindow class that sets up the GUI
 
@@ -22,6 +23,7 @@ class MainWindow(QMainWindow):
         font.setPointSize(20)
         self.label_header.setFont(font)
         self.open_button = QPushButton("Open Image", self)
+        self.open_button.clicked.connect(self.open_image)
         self.convert_button = QPushButton("Convert", self)
 
         # Create image label and text box
@@ -57,6 +59,37 @@ class MainWindow(QMainWindow):
         widget = QWidget(self)
         widget.setLayout(layout_main)
         self.setCentralWidget(widget)
+
+    # Method to open image file
+    def open_image(self):
+        # Open a file dialog to select an image
+        self.file = QFileDialog.getOpenFileName(
+            self, "Open Image", "", "Images (*.png *.xpm *.jpg *.bmp *.gif);;All Files (*)")[0]
+
+        # If a file is selected
+        if self.file:
+            # Set the file path as the previous file
+            self.previous = self.file
+
+            # If the file path is longer than 30 characters
+            if len(self.file) >= 30:
+                # Truncate the file path and set it as the text on the open button
+                self.open_button.setText(
+                    self.file[:30] + "..." + self.file[-3:])
+            else:
+                # Set the file path as the text on the open button
+                self.open_button.setText(self.file)
+
+            # Open the image file using PIL
+            self.image = Image.open(self.file)
+            # Create a QPixmap from the image file
+            pixmap = QPixmap(self.file)
+            # Set the pixmap on the image label
+            self.label_image.setPixmap(pixmap)
+        else:
+            # If a file was previously selected, set it as the current file
+            if self.previous != "":
+                self.file = self.previous
 
 
 # Create a QApplication
